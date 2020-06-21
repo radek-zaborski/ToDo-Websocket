d7915d042eab9ecb40d741893d57559e07de7565
unknown <radex171@gmail.com>
2020-06-21 17:57:07 +0200

    init project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const socket = require('socket.io');
+
+let tasks = [];
+
+const app = express();
+const server = app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running...');
+  });
+  
+  app.use((req, res) => {
+    res.status(404).send({ message: 'Not found...' });
+  });
